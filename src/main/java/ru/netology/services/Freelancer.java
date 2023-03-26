a09ce228aa174df2262e12dd169689a4e48836b8
Vitaliy Z <vv-z@mail.ru>
2023-03-26 15:34:54 +0300

    Corrected test commit

--- src/main/java/ru/netology/services/Freelancer.java
+++ src/main/java/ru/netology/services/Freelancer.java
@@ -5,7 +5,7 @@ public class Freelancer {
         int counter = 0;
         int money = 0;
 
-        for (int i = 0; i <= 12; i++) {
+        for (int i = 0; i < 12; i++) {
             if (money >= threshold) {
                 money = money - expenses;
                 money = money / 3;
